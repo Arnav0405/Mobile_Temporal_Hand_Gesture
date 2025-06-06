7a47a45d63305c2d21e9130a9985a2088ad1503d
arnav0405 <arnavwaghdhare@gmail.com>
2025-06-06 07:50:33 +0530

    Export to TorchScript

--- export_to_onnx.py
+++ export_to_onnx.py
@@ -10,18 +10,19 @@ dummy_input = torch.randn(1, 30, 63)
 
 traced_model = torch.jit.trace(model, dummy_input)
 
-torch.onnx.export(
-    traced_model,               # PyTorch model
-    dummy_input,        # Input tensor or tuple of tensors
-    "tcn_model.onnx",           # Output file path
-    export_params=True,  # Store model weights
-    opset_version=12,    # ONNX version
-    do_constant_folding=True,  # Optimize constants
-    input_names=['input'],     # Input names
-    output_names=['output'],   # Output names
-    dynamic_axes={
-        'input': {0: 'batch_size', 1: 'sequence_length'},
-        'output': {0: 'batch_size'}
-    }
-)
+traced_model.save("tcn_model.pt")
+# torch.onnx.export(
+#     traced_model,               # PyTorch model
+#     dummy_input,        # Input tensor or tuple of tensors
+#     "tcn_model.onnx",           # Output file path
+#     export_params=True,  # Store model weights
+#     opset_version=12,    # ONNX version
+#     do_constant_folding=True,  # Optimize constants
+#     input_names=['input'],     # Input names
+#     output_names=['output'],   # Output names
+#     dynamic_axes={
+#         'input': {0: 'batch_size', 1: 'sequence_length'},
+#         'output': {0: 'batch_size'}
+#     }
+# )
 
